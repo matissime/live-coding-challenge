458c24e2104cd7158da18cbc4f1a03a11b1d8f74
linternational <cabreldiffo20@gmail.com>
2025-04-19 12:56:35 +0000

    Voila mon projet

--- app.js
+++ app.js
@@ -11,43 +11,62 @@ const db = mysql.createConnection({
     database: 'student_db'
   });
   
-  db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to the database');
-  });
+db.connect((err) => {
+  if (err){
+    console.error('Database connection failed:',err);
+    process.exit(1);
+  }
+
+console.log('connection database ');
+
+});
 
 // Inscription d'un utilisateur
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
-  const query = `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`;
+  const query = `INSERT INTO users (username, password) VALUES (?, ?)`;
   
-  db.query(query, (err, result) => {
-    if (err) throw err;
-    res.send('User registered successfully!');
+  db.query(query, [username,password],(err,result) => {
+    if (err) return res.status(500).send(err);
+      res.status(201).send('user registred succesfully');
   });
 });
 
 // Récupération des notes
 app.get('/grades', (req, res) => {
   const { username } = req.query;
-  const query = `SELECT * FROM grades WHERE username = '${username}'`;
-  console.log('Executing query:', query);
-  
-  try {
-    db.query(query, (err, result) => {
-      if (err) {
-        console.error('SQL Error:', err.message);
-        return res.status(500).json({ error: err.message, query: query });
-      }
-      res.json(result);
+  const query = `SELECT * FROM grades WHERE username = ?`;
+  db.query(query, [username],(err ,result)=>{
+    if (err){
+      console.error('SQL Error:',err.message);
+      return res.status(500).json({error:err.message});
+    }
+    res.json(result);
     });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: error.message });
-  }
+
+  });
+
+  app.post('/grades',(req,res)=>{
+     const{username,grade}=req.body;
+
+     if (typeof grade !== 'number' || grade <0 || grade >20){
+      return res.status(400).json({message:'grade incorect '});
+      
+     }
+
+    const query ='INSERT USERNAME ,grades (username,grade) VALUES (?,?)'; 
+
+      db.query = (query, [username, grade],(err, result)=>{
+        if(err) throw err ;
+
+      res.status(201).json({message: 'grade ajouter',id:result.insertId});
+      
+    });
+ 
 });
 
 // Démarrage du serveur
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server running on port ${PORT}');
 });
